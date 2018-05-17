3689cf4670a4c89fc4e7375ba02a033e3cd03ed6
gupta583 <32807130+gupta583@users.noreply.github.com>
2018-05-17 17:26:52 -0400

    Update fig2.m

--- fig2.m
+++ fig2.m
@@ -1,7 +1,7 @@
 clear all;
 figure(1);
 clf;
-repvariance = dlmread('repvarianceh1r01.dat');
+repvariance = dlmread('fig2b.dat');   % genrate data from fig2b_data folder
 
 nstar = 15;
 tstar = 20;
@@ -55,37 +55,25 @@ subplot(1,2,1);
 a0s = logspace(log10(1),log10(15),150);
 actks = logspace(log10(0.2),log10(1.0),150);
 
-actvariance = dlmread('actvarianceh1a01.dat');
+actvariance = dlmread('fig2a.dat');    % generate the data from folder fig2a/
 
 axa = 1:0.1:15;
-aya = (axa + nstar/2)/tstar;
+aya = (axa + nstar/2)/tstar;           % curve for minima, analytics
 
 
-
-%imagesc(log10(a0s),log10(actks*tstar),(actvariance*nstar/tstar^2)');
 contourf(log10(a0s),log10(actks*tstar),actvariance'*nstar/tstar^2)
 hold on;
 
 plot(log10(axa),log10(aya*tstar),'--','color','white','linewidth',2);
-%xlim(log10([.1 15]))
 ylim(log10([4 20]))
 axis('square')
 
-%text(log10(0.5),log10(1.13*tstar),'A','fontsize',14)
 xlabel('Half-maximal number, $K$','Interpreter','latex');
 ylabel('Production rate, $kt_*$','Interpreter','latex');
 title('Activator')
 c = colorbar;
-%c.Label.String = '\sigma_t^2';
 ylabel(c,'Timing variance, $\sigma_t^2x_*/t_*^2$','fontsize',11,...
  'Interpreter','latex','fontsize',15)
-
-%set(gca,'XTickLabel',{  '10^0', '10^1' });
-%set(gca,'XTick',[ log10(10^0) log10(10^1) ]);
-
-
-%set(gca,'YTickLabel',{'10^{-2}', '10^{-1}', '10^0'  });
-%set(gca,'YTick',[log10(10^-2) log10(10^-1) log10(10^0)   ]);
 
 
 set(gca,'ydir','normal',...
