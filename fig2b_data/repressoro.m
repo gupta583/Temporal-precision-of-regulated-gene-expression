e5b1278b38516a957c5d185ffda639dc6f9db925
gupta583 <32807130+gupta583@users.noreply.github.com>
2018-05-17 17:23:42 -0400

    Update repressoro.m

--- fig2b_data/repressoro.m
+++ fig2b_data/repressoro.m
@@ -2,29 +2,26 @@ clear all;
 
 nstar = 15;
 tstar = 20;
-Nr = 365;
+Nr = 15;
 
-r0s = logspace(log10(45.0),log10(50),20);
-ks = logspace(log10(0.09),log10(0.5),20);
+r0s = logspace(log10(1),log10(15),300);    %Half maxima K
+ks = logspace(log10(0.01),log10(5),300);    % Decay rate, \mu
 
 ncolumn = (Nr+1)*nstar;
 nrow = ncolumn;
 
 P0 = zeros(nrow,1);
 P0(Nr+1,1) = 1;
-matrixA(2,1,0,1,2)
-pause
 
 
 for rr=1:length(r0s)
     r0 = r0s(rr)
     for kk=1:length(ks)
         k = ks(kk)
-        matrixA(Nr,r0,k,alpha,nstar)
-        pause;
+      
         l = @(alpha) (1*transpose(matrixU(Nr,r0,alpha,nstar))*...
                   (inv(matrixA(Nr,r0,k,alpha,nstar)))^2*P0 - tstar)^2;
-              a1 = fminbnd(l,0,50000);
+              a1 = fminbnd(l,0,50000);    % set the target condition at t = t_* | x = x_*
               
  mean = 1*transpose(matrixU(Nr,r0,a1,nstar))*...
           (inv(matrixA(Nr,r0,k,a1,nstar)))^2*P0;
@@ -41,6 +38,5 @@ imagesc(log10(r0s),log10(ks),variance')
 xlabel('log_{10}r_0')
 ylabel('log_{10}k')
 colorbar;
-%print(gcf,'-depsc', 'mat9-var.eps')
-%dlmwrite('varianceh1300point.dat',variance)
+dlmwrite('fig2b.dat',variance)
 
