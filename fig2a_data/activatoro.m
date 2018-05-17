d1345d4665422dc70f1f05f6b32c38249eb74148
gupta583 <32807130+gupta583@users.noreply.github.com>
2018-05-17 17:20:12 -0400

    Update activatoro.m

--- fig2a_data/activatoro.m
+++ fig2a_data/activatoro.m
@@ -3,8 +3,8 @@ clear all;
 nstar = 15;
 tstar = 20;
 
-a0s = logspace(log10(290),log10(292.5),2);     % 1 to 15
-ks = logspace(log10(0.2),log10(15.0),2);   %intitial 2 --> 150    0.2 to 1
+a0s = logspace(log10(1),log10(15),150);     % K, Half maxima
+ks = logspace(log10(0.2),log10(10),150);    % Production rate, k
 
 for aa=1:length(a0s)
     a0 = a0s(aa);
@@ -13,21 +13,21 @@ for aa=1:length(a0s)
       k = ks(kk);
       
       
-      Nc = fix(k*tstar + 10*sqrt(k*tstar));
+      Nc = fix(k*tstar + 10*sqrt(k*tstar));     %cutoff for activator mean + 10*standard deviation
       ncolumn = (Nc+1)*nstar;
       nrow = ncolumn;
       
       P0 = zeros(nrow,1);
       P0(1,1) = 1;
       
-      
+      % fix the target condition by finding correct alpha
        l = @(alpha) (1*transpose(matrixU(a0,k,alpha,nstar,tstar))*...
-                  (inv(matrixA(a0,k,alpha,nstar,tstar)))^2*P0 - tstar)^2;
-       a1 = fminbnd(l,0,500000);
+                  (inv(matrixA(a0,k,alpha,nstar,tstar)))^2*P0 - tstar)^2;    
+       a1 = fminbnd(l,0,500000);          
        
-       mean = 1*transpose(matrixU(a0,k,a1,nstar,tstar))*...
+       mean = 1*transpose(matrixU(a0,k,a1,nstar,tstar))*...          % mean first passage time
               (inv(matrixA(a0,k,a1,nstar,tstar)))^2*P0;
-      mean2= -1*2*transpose(matrixU(a0,k,a1,nstar,tstar))*...
+       mean2= -1*2*transpose(matrixU(a0,k,a1,nstar,tstar))*...
               (inv(matrixA(a0,k,a1,nstar,tstar)))^3*P0;
           
       variance(aa,kk) = mean2 - mean^2;
@@ -43,8 +43,7 @@ imagesc(log10(a0s),log10(ks),variance')
 xlabel('log_{10}a0')
 ylabel('log_{10}k')
 colorbar;
-%print(gcf,'-depsc', 'mat10var.eps')
-%dlmwrite('actvarianceh1.dat',variance)
+dlmwrite('fig2a.dat',variance)
 
 
 
